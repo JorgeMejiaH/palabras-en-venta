222be654700e9de205d5bb2719da8e4cef71e98f
SantiagoAR2000 <95257108+SantiagoAR2000@users.noreply.github.com>
2024-03-15 10:36:21 -0500

    Add files via upload
    Intento de pie de paginas para pagos

---
+++ src/components/FootPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img
+        src="@/assets/WhiteBackground.png"
+        alt="fondo blanco"
+        class="white-background"
+        />
+    </div> 
+    <div>
+        <img src="@/assets/Group55.png" alt="Logo seguridad de pagos" class="img-pagos" />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
