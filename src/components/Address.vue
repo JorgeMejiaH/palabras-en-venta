3fc40f7370447ebd14be34499ae067755c857ebf
SantiagoAR2000 <95257108+SantiagoAR2000@users.noreply.github.com>
2024-05-01 18:12:34 -0500

    Add files via upload

---
+++ src/components/Address.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <Options/>
+        <img
+        src="@/assets/config-background.png"
+        alt="Fondo de la pagina"
+        class="config-background"
+        />
+        <div class="container-address">
+            <AddressInfo/>
+        </div>
+    </div>
+</template>
+
+<script>
+import  Options  from "@/components/Account/Options.vue";
+import AddressInfo from "@/components/Account/AddressInformation.vue";
+export default{
+    components: {Options, AddressInfo},
+};
+</script>
+
+<style>
+.config-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 200%;
+    z-index: -2;
+    background-color: #58595B;
+}
+
+.container-address{
+    position: absolute;
+    justify-content: flex-start;
+    top: 30%;
+    right: 0;
+    width: 78%;
+    height: 1000px;
+    background-color: white;
+    z-index: 1;
+    border-radius: 20px;
+}
+
+</style>
